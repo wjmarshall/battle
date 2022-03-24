68d0bb48007c52491340167ffcba94bea52226ab
William Marshall <w.marshall@elsevier.com>
2022-03-24 12:10:57 +0000

    Added web_helpers.rb and refactored specs

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,10 +1,6 @@
 RSpec.feature "Enter names" do
   scenario "Players can enter and see their names" do
-    visit("/")
-    fill_in(:player_1, with: "Ian")
-    fill_in(:player_2, with: "Fran")
-    click_button("Submit")
-    save_and_open_page
+    sign_in_and_play
     expect(page).to have_content("Ian vs. Fran")
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,10 +1,6 @@
 RSpec.feature "Display hit points" do
   scenario "see player 2's hit points" do
-    visit "/"
-    fill_in(:player_1, with: "Tom")
-    fill_in(:player_2, with: "Jerry")
-    click_button("Submit")
-    save_and_open_page
+    sign_in_and_play
     expect(page).to have_content("Jerry: 60 HP")
   end
 end
