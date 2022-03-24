2a9ebe3ef60aad00678b5c80b307375f11211741
William Marshall <w.marshall@elsevier.com>
2022-03-24 10:23:40 +0000

    Project setup

---
+++ app.rb
@@ -0,0 +1,20 @@
+require "sinatra/base"
+require "sinatra/reloader"
+
+class Battle < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get "/" do
+    "Hello Battle!"
+    "Testing infrastructure working!"
+  end
+
+  run! if app_file == $0
+end
+
+
+
+
+
